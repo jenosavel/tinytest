cea120885b2020d08c9b5c66096ebe4736f2fe1c
jenosavel <jenosavel@gmail.com>
2015-08-09 01:23:26 -0700

    Minor reporting clean-ups

--- reporting.py
+++ reporting.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 class Status:
 
     BLOCKED    = 'blocked'
@@ -13,7 +15,7 @@ class Report(object):
 
     def __init__(self):
 
-        self.__results = {}
+        self.__results = OrderedDict()
         self.__failure = None
 
     def initialize(self, tests):
@@ -51,17 +53,17 @@ class Report(object):
 
         if self.__results:
 
-            statusPadding = max(len(result[Report.STATUS]) for result in self.__results.values()) + 2
-            descPadding   = max(len(test) for test in self.__results.keys())
+            statusPadding = max(len(result[Report.STATUS]) for result in self.__results.values()) + len('[]')
+            descPadding   = max(len(test) for test in self.__results.keys()) + len('should ')
 
-            for test, result in sorted(self.__results.iteritems(), key = lambda tuple: tuple[1][Report.STATUS]):
+            for test, result in sorted(self.__results.items()):
 
                 status = result[Report.STATUS]
                 failure = ' // {0}'.format(result[Report.REASON]) if status is Status.FAIL or status is Status.BLOCKED else ''
 
                 results.append('{0} {1} {2}'.format(
                     '[{0}]'.format(status.upper()).ljust(statusPadding),
-                    test.ljust(descPadding),
+                    'should {0}'.format(test).ljust(descPadding),
                     failure
                 ))
 
